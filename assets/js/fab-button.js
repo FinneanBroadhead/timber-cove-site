dfa0da05079fd5b0d4a8bedef6c06a9b96664ca3
FinneanBroadhead <finnean@flizzet.com>
2019-06-10 20:16:14 -0700

    finished contact page, added floating action button to smooth navigation and implemented new styles and minor touches.

---
+++ assets/js/fab-button.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.fixed-action-btn');
+	var instances = M.FloatingActionButton.init();
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+	$('.fixed-action-btn').floatingActionButton();
+});
+var instance = M.FloatingActionButton.getInstance(elem);
+
+//  jQuery Method Calls
+// 	You can still use the old jQuery plugin method calls.
+// 	But you won't be able to access instance properties.
+
+	$('.fixed-action-btn').floatingActionButton('instance.open();');
+	$('.fixed-action-btn').floatingActionButton('instance.close();');
